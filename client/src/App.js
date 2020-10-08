9fde981dabfd5b6213dd9ac3458d5a517572eddf
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-10-08 23:36:29 +0300

    Add User Resource

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import restProvider from "ra-data-simple-rest";
 import PostList from "./components/PostList";
 import PostCreate from "./components/PostCreate";
 import PostEdit from "./components/PostEdit";
+import UserList from "./components/UserList";
+import UserCreate from "./components/UserCreate";
+import UserEdit from "./components/UserEdit";
 
 function App() {
   return (
@@ -13,6 +16,12 @@ function App() {
         list={PostList}
         create={PostCreate}
         edit={PostEdit}
+      />
+      <Resource
+        name="users"
+        list={UserList}
+        create={UserCreate}
+        edit={UserEdit}
       />
     </Admin>
   );
